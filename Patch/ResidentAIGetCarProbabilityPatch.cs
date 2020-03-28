cb5016c27cabffbeba8ca016417daa381cf5b702
pcfantasy <pcfantasy@live.cn>
2020-03-28 12:12:28 +0800

    remove restrict for resident to use vehicle
    currently unused

--- Patch/ResidentAIGetCarProbabilityPatch.cs
+++ Patch/ResidentAIGetCarProbabilityPatch.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 
 namespace RealCity.Patch
 {
-    [HarmonyPatch]
+    //[HarmonyPatch]
     public class ResidentAIGetCarProbabilityPatch
     {
         public static MethodBase TargetMethod()
